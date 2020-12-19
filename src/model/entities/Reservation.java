f7284ae2cf38112913d2f0b299ab1370f93d05ff
Marcelo1985-06 <marceloliveira_lima@outlook.com>
2020-12-19 17:40:35 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -19,18 +19,22 @@ public class Reservation {
     }
 
     public Integer getRoomNumber() {
+
         return roomNumber;
     }
 
     public void setRoomNumber(Integer roomNumber) {
+
         this.roomNumber = roomNumber;
     }
 
     public Date getCheckIn() {
+
         return checkIn;
     }
 
     public Date getCheckOut() {
+
         return checkOut;
     }
 
@@ -39,9 +43,18 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut){
+    public String updateDates(Date checkIn, Date checkOut){
+
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)){
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)){
+            return "Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return  null;
     }
 
     @Override
